558c67c25055b493bf419e4dd89b77f85eb6442f
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-07-08 09:12:51 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+            transactions.Add(("Yo-yo", 9.94, 1));
+            transactions.Add(("screw", 0.0214, 100));
+            transactions.Add(("hammer", 19.95, 3));
+            transactions.Add(("Tape Measure", 9.95, 5));
+            transactions.Add(("bullfrog", 6.00, 1));
+            
+            double total = 0.0;
+            int qty = 0;
+            foreach((string product, double amount, int quantity) t in transactions){
+                total = total + (t.quantity * t.amount);
+                qty = qty + t.quantity;
+            }
+            Console.WriteLine($"Total inv moved is {qty} items");
+            Console.WriteLine($"Revenue for day is {total}");
+        }
+    }
+}
